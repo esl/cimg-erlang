b527d29b1dbd5ce42fa32356bff471ca7b8d8522
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2023-03-15 15:58:37 +0100

    Make base image version configurable and frozen

--- compile.sh
+++ compile.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-docker build --progress=plain --build-arg OTP_VERSION -t cimg-erlang-builder -f Dockerfile-compile .
+docker build --progress=plain --build-arg BASE_VERSION --build-arg OTP_VERSION \
+    -t cimg-erlang-builder -f Dockerfile-compile .
 BUILDER=`docker create cimg-erlang-builder`
 docker cp $BUILDER:/home/circleci/builds .
 docker rm $BUILDER
