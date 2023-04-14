a736e75c070b46cde137bfe8f0744369853eae04
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2023-04-14 15:42:03 +0200

    Disable provenance to make the docker manifest backwards-compatible
    By default buildx creates OCI manifests, which cause a bug when you try to:
    - Inspect the docker manifest - even in the latest docker version
    - Pull the docker image in older docker - reproduced in 17.x
    
    See docker/buildx#1509 for details.

--- circleci-build.sh
+++ circleci-build.sh
@@ -9,4 +9,4 @@ fi
 docker buildx build --platform linux/amd64,linux/arm64 \
     --build-arg BASE_VERSION --build-arg OTP_VERSION \
     -t mongooseim/cimg-erlang:$OTP_VERSION \
-    --progress=plain -f Dockerfile $PUSH .
+    --progress=plain -f Dockerfile --provenance=false $PUSH .
