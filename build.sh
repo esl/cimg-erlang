3e3db2556e584f2e332ab9b20a911aaf429595ca
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-10-24 14:47:35 +0200

    Use a common helper in build.sh and circleci-build.sh

--- build.sh
+++ build.sh
@@ -2,19 +2,11 @@
 
 set -euo pipefail
 
-export BASE_IMAGE=${BASE_IMAGE:-cimg/base:current}
-BASE_IMAGE_NAME=${BASE_IMAGE%:*}
-BASE_IMAGE_VERSION=${BASE_IMAGE#*:}
+source tools/utils.sh
 
 PROGRESS=auto ./compile.sh
 
-if [ "${BASE_IMAGE%:*}" = "cimg/base" ]; then
-    USER=circleci # CircleCI convenience images require a special user
-    IMAGE_TAG=${OTP_VERSION}
-else
-    USER=root
-    IMAGE_TAG=${BASE_IMAGE_NAME}-${BASE_IMAGE_VERSION}-${OTP_VERSION}
-fi
+set_up_build
 
 docker build --build-arg BASE_IMAGE --build-arg OTP_VERSION --build-arg USER \
     -t cimg-erlang:$IMAGE_TAG .
